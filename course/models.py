d6a99e9a66dbdb9843fb8bfc43b3216f8a890e5f
Hevannder Rubian <hevannder.albuquerque@fpf.br>
2022-07-16 18:05:54 -0400

    aual 2.1 - criação de modelbase

--- course/models.py
+++ course/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class Course(models.Model):
+class ModelBase(models.Model):
     id = models.AutoField(primary_key=True, null=False)
-    name = models.CharField(max_length=40, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True, null=False)
+
+    class Meta:
+        abstract = True
+
+class Course(ModelBase):
+    name = models.CharField(max_length=40, null=False)
 
     class Meta:
         db_table = 'course'

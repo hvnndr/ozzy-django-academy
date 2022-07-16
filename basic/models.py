7f7aabdbc429893342fe9872788a04669550cfbb
Hevannder Rubian <hevannder.albuquerque@fpf.br>
2022-07-16 13:11:17 -0400

    aula 1.2 - criando campo gender em modelo employee

--- basic/models.py
+++ basic/models.py
@@ -6,6 +6,7 @@ class Employee(models.Model):
     id = models.AutoField(primary_key=True, null=False)
     name = models.CharField(max_length=100, null=False)
     salary = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    gender = models.CharField(max_length=2, null=False, default='M')
 
     class Meta:
         db_table = 'employee'

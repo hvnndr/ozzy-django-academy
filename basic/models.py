31ffc890449b2e2e42eeab07af487282d938df55
Hevannder Rubian <hevannder.albuquerque@fpf.br>
2022-07-16 12:48:07 -0400

    aula 1.1 - criando o modelo employee e gerando migration

--- basic/models.py
+++ basic/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    id = models.AutoField(primary_key=True, null=False)
+    name = models.CharField(max_length=100, null=False)
+    salary = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+
+    class Meta:
+        db_table = 'employee'
+        managed = True
+

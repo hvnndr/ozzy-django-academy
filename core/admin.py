413290b0bdf4cdaf3203fbef9ea49959b87c2a26
Hevannder Rubian <hevannder.albuquerque@fpf.br>
2022-07-16 22:54:14 -0400

    aula 2.16 - criando acesso de modelo department e marital status no django admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from core import models
 
 # Register your models here.
+
+@admin.register(models.Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'active', 'modified_at']
+    list_display_links = ['id', 'name', 'active', 'modified_at']
+    search_fields = ['id', 'name', 'active']
+    list_filter = ['active']
+
+@admin.register(models.MaritalStatus)
+class MaritalStatusAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'active', 'modified_at']
+    list_display_links = ['id', 'name', 'active', 'modified_at']
+    search_fields = ['id', 'name', 'active']
+    list_filter = ['active']
+    

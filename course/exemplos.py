df31cfe19d23e8bdedf183b459c174071dfba911
Hevannder Rubian <hevannder.albuquerque@fpf.br>
2022-07-23 00:37:34 -0400

    aula 3.2 - adicionando alunos e adicionando tods alunos a todos os cursos

--- course/exemplos.py
+++ course/exemplos.py
@@ -17,3 +17,23 @@ def cadastra_alunos_2():
 
     for a in alunos:
         models.Student.objects.create(name=a, course=curso)
+
+
+def cadastra_somente_alunos():
+    alunos = ['Hev', 'Cris', 'Rezende', 'Breno', 'Vini']
+    for a in alunos:
+        aluno = models.Student()
+        aluno.name = a
+        aluno.save()
+
+def associa_todos_cursos_a_todos_alunos():
+    students = models.Student.objects.all()
+    course = models.Course.objects.all()
+
+    for s in students:
+        for c in course:
+            cs = models.StudentCourse()
+            cs.student = s
+            cs.course = c
+            cs.save()
+    
